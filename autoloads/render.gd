56eeb24187dd8b5613f59d80ab0c2b2fcccfae64
Antlux <gurlux@icloud.com>
2025-01-27 03:10:03 +0100

    feat: Added 'current_frame' variable

--- autoloads/render.gd
+++ autoloads/render.gd
@@ -1,8 +1,8 @@
 extends Node
 
 signal changed(render_material: ShaderMaterial)
-signal updated(timestamp: float)
-signal resized(new_size: Vector2i)
+signal updated(timestamp: float, current_frame: int)
+signal resized(from: Vector2i, to: Vector2i)
 
 var render_material : ShaderMaterial:
 	set(value):
@@ -11,18 +11,20 @@ var render_material : ShaderMaterial:
 			changed.emit(value)
 var rendering := false
 var current_time: float = 0.0
+var current_frame: int = 0
 
 var size := Vector2i(512, 512) :
 	set(value):
 		if size != value:
+			var previous := size
 			size = value
-			resized.emit(value)
+			resized.emit(previous, value)
 var duration: float = 1.0
 var frame_count: int = 1
 
 func update(timestamp: float) -> void:
 	render_material.set_shader_parameter("outside_time", timestamp)
-	updated.emit(timestamp)
+	updated.emit(timestamp, current_frame)
 
 func _process(delta: float) -> void:
 	
@@ -31,6 +33,7 @@ func _process(delta: float) -> void:
 	
 	if not rendering:
 		current_time = fmod(current_time + delta, duration)
+		current_frame = floori(current_time / duration * frame_count)
 		var frame_rate = frame_count / duration;
 		var timestamp = floor(current_time * frame_rate) /  frame_rate
 		update(timestamp)
--- scenes/UI/render_progress_bar.gd
+++ scenes/UI/render_progress_bar.gd
@@ -3,11 +3,11 @@ extends ProgressBar
 func _ready() -> void:
 	Global.render_settings.changed.connect(_on_render_settings_changed)
 	Render.updated.connect(_on_render_updated)
-	self.max_value = Global.render_settings.duration
+	self.max_value = Global.render_settings.frame_count - 1
 
 
 func _on_render_settings_changed() -> void:
-	self.max_value = Global.render_settings.duration
+	self.max_value = Global.render_settings.frame_count - 1
 
-func _on_render_updated(timestamp: float) -> void:
-	self.value = timestamp
+func _on_render_updated(_timestamp: float, frame_idx: int) -> void:
+	self.value = frame_idx
